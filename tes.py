6c41d226579f4c9475fe775b47947f27d4dc0198
inikanipp <143859651+inikanipp@users.noreply.github.com>
2023-09-02 10:45:42 +0700

    first commit
    gada

---
+++ tes.py
@@ -0,0 +1,56 @@
+# 1.integer
+apel = 52
+print(apel)
+print(521)
+
+# 2.float
+nama_apel = 3.14
+print(nama_apel)
+
+# 3.string
+a1pel = "makan" 
+print(a1pel)
+
+# 4.boolean
+apel_benar = True
+apel_salah = False
+print(type(apel_benar))
+print(apel_benar)
+
+# 5.list
+list_apel = ['merah', 'kuning', 'hijau']
+list_apel1 = ['manis', '12', 'kuning', '1.2']
+print(type(list_apel))
+print(list_apel1)
+
+# 6.tuple
+ini_tuple = ('kuda', 'kucing', 'macan')
+ini_jumlah = ('1', '2',"kucing", 'kucing')
+print(ini_jumlah) 
+
+# 7.dictioary
+apel_dic = {'nama': 'apel', 'asal' : 'malang'}
+print(apel_dic['asal'])
+
+# cek type
+print(type(nama_apel))
+
+# operator aritmatika
+print(3+4)
+print(9-7)
+print(9*3)
+print(18/3)
+
+a = 5
+b = 16
+c = 3
+print(a+c*b)
+
+# modulus
+print(b % c)
+
+# pangkat
+print(5 ** 3)
+
+# hasil bagi bulat
+print(b // c)
